31f7cad44791876abcd11ded935aac5994717f8c
ShankarAswal <shankaraswal@gmail.com>
2019-11-29 01:57:02 +0530

    modified by sasal user

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
       <div className="container">
     
          <div className="jumbotron">
-            <h1 className="display-4 ">Home!</h1>
+            <h1 className="display-4 ">Home Updated!</h1>
 
             <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                 <hr class="my-4" />
